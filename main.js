d4f0487948ca05da7eabef7600c0d7a3820b35bf
irakli85 <iberidze38@gmail.com>
2022-08-24 17:31:46 +0400

    firs change

---
+++ main.js
@@ -0,0 +1,40 @@
+let docInput = document.querySelector("#bill");
+let factInput = document.querySelector("#people");
+let numberOutput = document.querySelector("#tip-per-person");
+let percentOutput = document.querySelector("#total-per-person");
+let reset = document.querySelector("#reset");
+
+
+
+let docValue;
+let factValue;
+let x;
+let y;
+
+function docInputFn(){
+    docValue = docInput.value;
+    console.log(docValue);
+}
+
+function factInputFn(){
+    factValue = factInput.value;
+    console.log(factValue);
+    x = +docValue - +factValue;
+    y = x / +docValue * 100;    
+    numberOutput.innerText = x + "kg";
+    percentOutput.innerText = y.toFixed(2) + "%";
+}
+
+function resetFn(){
+    docInput.value= "";
+    factInput.value = "";
+    numberOutput.innerText = "0.00";
+    percentOutput.innerText = "0.00";
+}
+
+
+
+docInput.addEventListener("input", docInputFn);
+factInput.addEventListener("input", factInputFn);
+reset.addEventListener("click", resetFn);
+            
